82e5a6c5bd6f22030129f7dbe142096e7391c028
NexusIsReal <nexusnirob3000@gmail.com>
2025-03-19 00:28:33 +0600

    .

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: ['next/core-web-vitals'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    overrides: [
+      {
+        files: ['**/app/api/**/*.ts'],
+        rules: {
+          '@typescript-eslint/no-explicit-any': 'off',
+          '@typescript-eslint/ban-types': 'off',
+        },
+      },
+    ],
+  };
